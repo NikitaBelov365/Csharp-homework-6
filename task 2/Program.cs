abb228bb8e9e219752d10879306f33334181e44a
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-02 15:08:25 +0300

    solution modified

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -2,21 +2,21 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 
-int InputCoord(string msg)
+double InputCoord(string msg)
 {
     System.Console.WriteLine(msg);
-    return Convert.ToInt32(Console.ReadLine());
+    return Convert.ToDouble(Console.ReadLine());
 }
 
-int b1 = InputCoord("Input b1");
-int k1 = InputCoord("Input k1");
-int b2 = InputCoord("Input b2");
-int k2 = InputCoord("Input k2");
+double b1 = InputCoord("Input b1");
+double k1 = InputCoord("Input k1");
+double b2 = InputCoord("Input b2");
+double k2 = InputCoord("Input k2");
 
-(int, int) intersectionPoint(int b1, int k1, int b2, int k2)
+(double, double) intersectionPoint(double b1, double k1, double b2, double k2)
 {
-    int x = (b2 - b1) / (k1 - k2);
-    int y = k1 * x + b1 - k2 * x - b2;
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
     return (x, y);
 }
 
